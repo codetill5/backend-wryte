3278255e5bdd1a04748d253e56e65ddb5e02cfdf
codetill5 <mehrashubham216@gmail.com>
2022-08-25 16:36:48 +0530

    user model

---
+++ models/user.js
@@ -0,0 +1,67 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide a name"],
+    maxLength: [40, "Name Should be under 40 characters"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide a email"],
+    validate: [validator.isEmail, "Please enter a valid email"],
+    unique: true,
+  },
+  walletAddress: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  role: {
+    type: String,
+    default: "user",
+  },
+  profileImg: {
+    id: {
+      type: String,
+      required: true,
+    },
+    secure_url: {
+      type: String,
+      required: true,
+    },
+  },
+  coverImg: {
+    id: {
+      type: String,
+      required: true,
+    },
+    secure_url: {
+      type: String,
+      required: true,
+    },
+  },
+//   bookmarks: {
+//     type: Array,
+//     default: "user",
+//   },
+//   followers: {
+//     type: Array,
+//     default: "user",
+//   },
+//   followings: {
+//     type: Array,
+//     default: "user",
+//   },
+  role: {
+    type: String,
+    default: "user",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
